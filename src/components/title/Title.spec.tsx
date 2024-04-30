f9d7db512e852542742d00fa7ee48954f56f2504
Michael Bradvica <mjbradvica@gmail.com>
2024-04-30 15:41:28 -0400

    fix test coverage

--- src/components/title/Title.spec.tsx
+++ src/components/title/Title.spec.tsx
@@ -13,4 +13,25 @@ describe("title", () => {
     expect(element).toHaveClass("title");
     expect(element).toHaveClass("is-1");
   });
+
+  it("two works correctly", () => {
+    const content = "title";
+
+    render(<Title type={TitleType.Two} content={content} />);
+
+    const element = screen.getByRole("heading", { name: content });
+
+    expect(element).toHaveClass("title");
+    expect(element).toHaveClass("is-2");
+  });
+
+  it("no value works correctly", () => {
+    const content = "title";
+
+    render(<Title type={999 as TitleType} content={content} />);
+
+    const element = screen.queryByRole("heading", { name: content });
+
+    expect(element).not.toBeInTheDocument();
+  });
 });
