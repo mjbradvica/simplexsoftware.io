f011045d431ea35dcfc0298a78b11b12a9821388
Michael B <mjbradvica@gmail.com>
2024-05-05 11:04:19 -0400

    working style lint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,9 @@ const App: React.FC = () => {
   return (
     <div>
       <Title content="Simplex-Software.io" titleType={TitleType.One} />
+      <button className="button is-tertiary" type="button">
+        Hi there!
+      </button>
     </div>
   );
 };
