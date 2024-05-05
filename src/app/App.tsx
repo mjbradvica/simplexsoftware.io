e25401110dc268baa9b46e6529c893546e04b531
Michael Bradvica <mjbradvica@gmail.com>
2024-05-05 15:39:00 -0400

    swap to bootstrap

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,14 @@ const App: React.FC = () => {
   return (
     <div>
       <Title content="Simplex-Software.io" titleType={TitleType.One} />
-      <button className="button is-tertiary" type="button">
-        Hi there!
+      <button className="btn btn-primary" type="button">
+        Primary
+      </button>
+      <button className="btn btn-secondary" type="button">
+        Secondary
+      </button>
+      <button className="btn btn-tertiary" type="button">
+        Tertiary
       </button>
     </div>
   );
