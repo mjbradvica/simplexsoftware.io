2b51a34b4e83d4e497181072e78a965d7c126c1a
Michael Bradvica <206864961@uniparks.com>
2025-07-21 10:09:41 -0400

    small updates

--- src/app/App.vue
+++ src/app/App.vue
@@ -10,6 +10,6 @@
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import HeaderView from "./features/layout/header/HeaderView.vue";
-import FooterView from "./features/layout/footer/FooterView.vue";
+import HeaderView from "../features/layout/header/HeaderView.vue";
+import FooterView from "../features/layout/footer/FooterView.vue";
 </script>
--- src/features/home/HomeView.vue
+++ src/features/home/HomeView.vue
@@ -1,7 +1,5 @@
 <template>
-  <h1 class="title">H1</h1>
-  <h2 class="title is-4">H2</h2>
-  <h3 class="title is-5">H3</h3>
+  <p>Home</p>
 </template>
 
 <script setup lang="ts"></script>
