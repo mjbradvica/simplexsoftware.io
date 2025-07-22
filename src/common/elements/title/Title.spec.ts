11e400a15857e9395a66a6de9370949499a118e3
Michael Bradvica <206864961@uniparks.com>
2025-07-22 11:24:08 -0400

    update title tests

--- src/common/elements/title/Title.spec.ts
+++ src/common/elements/title/Title.spec.ts
@@ -1,5 +1,47 @@
-import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/vue";
+import { describe, expect, it } from "vitest";
+import TitleElement from "./TitleElement.vue";
+import { TitleType } from "./TitleType";
 
 describe("title", () => {
-  it("works", () => {});
+  it("heading one is correct", () => {
+    render(TitleElement, {
+      props: {
+        type: TitleType.One,
+        content: "title",
+      },
+    });
+
+    expect(screen.getByRole("heading", { name: /title/i })).toHaveClass(
+      "title",
+    );
+  });
+
+  it("heading two is correct", () => {
+    render(TitleElement, {
+      props: {
+        type: TitleType.Two,
+        content: "title",
+      },
+    });
+
+    expect(screen.getByRole("heading", { name: /title/i })).toHaveClass(
+      "title",
+      "is-4",
+    );
+  });
+
+  it("heading three is correct", () => {
+    render(TitleElement, {
+      props: {
+        type: TitleType.Three,
+        content: "title",
+      },
+    });
+
+    expect(screen.getByRole("heading", { name: /title/i })).toHaveClass(
+      "title",
+      "is-5",
+    );
+  });
 });
